e51bd44dad74d515a65558beb6ba0c02b3c0b300
Leila <nb.leila10@gmail.com>
2019-05-11 18:43:55 -0500

    Add map to homepage

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactMapGl, { NavigationControl } from "react-map-gl";
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+const INITIAL_VIEWPORT = {
+  latitude: 29.6197,
+  longitude: -95.6349,
+  zoom: 13
+};
+
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  const [viewPort, setViewPort] = useState(INITIAL_VIEWPORT);
+
+  return (
+    <div className={classes.root}>
+      <ReactMapGl
+        width='100vw'
+        height='calc(100vh - 64px)'
+        mapStyle='mapbox://styles/mapbox/satellite-streets-v10'
+        // could use for example mapStyle='mapbox://styles/mapbox/satellite-v9'
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        onViewportChange={newViewPort => setViewPort(newViewPort)}
+        {...viewPort}
+      >
+        {/* Navigation Control */}
+        <div className={classes.navigationControl}>
+          <NavigationControl onViewportChange={newViewPort => setViewPort(newViewPort)} />
+        </div>
+      </ReactMapGl>
+    </div>
+  );
 };
 
 const styles = {
