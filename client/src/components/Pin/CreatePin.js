7e395cea8986683e9863280961d75d8cb8a581f5
Leila <nb.leila10@gmail.com>
2019-05-12 11:16:41 -0500

    Use Cloudinary to save the image when submit

--- client/src/components/Pin/CreatePin.js
+++ client/src/components/Pin/CreatePin.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from "react";
+import axios from "axios";
 import { withStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
@@ -17,9 +18,10 @@ const CreatePin = ({ classes }) => {
   const [image, setImage] = useState("");
   const [content, setContent] = useState("");
 
-  const handleSubmit = event => {
+  const handleSubmit = async event => {
     event.preventDefault();
-    console.log({ title, image, content });
+    const url = await handleImageUpload();
+    console.log({ title, image, content, url });
   };
 
   const handleDeleteDraft = () => {
@@ -29,6 +31,14 @@ const CreatePin = ({ classes }) => {
     dispatch({ type: "DELETE_DRAFT" });
   };
 
+  const handleImageUpload = async () => {
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "geopins");
+    data.append("cloud_name", "leila");
+    const res = await axios.post("https://api.cloudinary.com/v1_1/leila/image/upload", data);
+    return res.data.url;
+  };
   return (
     <form className={classes.form}>
       <Typography className={classes.alignCenter} component='h2' variant='h4' color='secondary'>
