e8f3457d03e72452dcf85c6ffc51745aae157555
Leila <nb.leila10@gmail.com>
2019-05-12 09:51:18 -0500

    Add draft pin when user clicks map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import ReactMapGl, { NavigationControl, Marker } from "react-map-gl";
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
@@ -6,6 +6,7 @@ import { withStyles } from "@material-ui/core/styles";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
 import PinIcon from "./PinIcon";
+import Context from "../context";
 
 const INITIAL_VIEWPORT = {
   latitude: 29.6197,
@@ -14,6 +15,7 @@ const INITIAL_VIEWPORT = {
 };
 
 const Map = ({ classes }) => {
+  const { state, dispatch } = useContext(Context);
   const [viewPort, setViewPort] = useState(INITIAL_VIEWPORT);
   const [userPosition, setUserPosition] = useState(null);
 
@@ -31,15 +33,25 @@ const Map = ({ classes }) => {
     }
   };
 
+  const handleMapClick = ({ lngLat, leftButton }) => {
+    if (!leftButton) return;
+    if (!state.draft) {
+      dispatch({ type: "CREATE_DRAFT" });
+    }
+    const [longitude, latitude] = lngLat;
+    dispatch({ type: "UPDATE_DRAFT_LOCATION", payload: { longitude, latitude } });
+  };
+
   return (
     <div className={classes.root}>
       <ReactMapGl
         width='100vw'
         height='calc(100vh - 64px)'
-        mapStyle='mapbox://styles/mapbox/satellite-streets-v10'
-        // could use for example mapStyle='mapbox://styles/mapbox/satellite-v9'
+        mapStyle='mapbox://styles/mapbox/streets-v10'
+        // could use for example mapStyle='mapbox://styles/mapbox/satellite-streets-v10'
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
         onViewportChange={newViewPort => setViewPort(newViewPort)}
+        onClick={handleMapClick}
         {...viewPort}
       >
         {/* Navigation Control */}
@@ -51,6 +63,13 @@ const Map = ({ classes }) => {
         {userPosition && (
           <Marker latitude={userPosition.latitude} longitude={userPosition.longitude} offsetLeft={-19} offsetTop={-37}>
             <PinIcon size={40} color='red' />
+          </Marker>
+        )}
+
+        {/* Draft Pin */}
+        {state.draft && (
+          <Marker latitude={state.draft.latitude} longitude={state.draft.longitude} offsetLeft={-19} offsetTop={-37}>
+            <PinIcon size={40} color='hotpink' />
           </Marker>
         )}
       </ReactMapGl>
