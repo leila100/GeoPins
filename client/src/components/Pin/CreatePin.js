e9042b633ba79e513dae69a76af13528c660999b
Leila <nb.leila10@gmail.com>
2019-05-12 10:48:38 -0500

    Add functionality to discard button

--- client/src/components/Pin/CreatePin.js
+++ client/src/components/Pin/CreatePin.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
@@ -8,30 +8,72 @@ import LandscapeIcon from "@material-ui/icons/LandscapeOutlined";
 import ClearIcon from "@material-ui/icons/Clear";
 import SaveIcon from "@material-ui/icons/SaveTwoTone";
 
+import Context from "../../context";
+
 const CreatePin = ({ classes }) => {
+  const { dispatch } = useContext(Context);
+
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log({ title, image, content });
+  };
+
+  const handleDeleteDraft = () => {
+    setTitle("");
+    setImage("");
+    setContent("");
+    dispatch({ type: "DELETE_DRAFT" });
+  };
+
   return (
     <form className={classes.form}>
       <Typography className={classes.alignCenter} component='h2' variant='h4' color='secondary'>
         <LandscapeIcon className={classes.iconLarge} /> Pin Location
       </Typography>
       <div>
-        <TextField name='title' label='Title' placeholder='Insert Pin title' />
-        <input accept='image/*' id='image' type='file' className={classes.input} />
+        <TextField name='title' label='Title' placeholder='Insert Pin title' onChange={e => setTitle(e.target.value)} />
+        <input
+          accept='image/*'
+          id='image'
+          type='file'
+          className={classes.input}
+          onChange={e => setImage(e.target.files[0])}
+        />
         <label htmlFor='image'>
-          <Button component='span' size='small' className={classes.button}>
+          <Button style={{ color: image && "green" }} component='span' size='small' className={classes.button}>
             <AddAPhotoIcon />
           </Button>
         </label>
       </div>
       <div className={classes.contentField}>
-        <TextField name='content' label='content' multiline rows='6' margin='normal' fullWidth variant='outlined' />
+        <TextField
+          name='content'
+          label='content'
+          multiline
+          rows='6'
+          margin='normal'
+          fullWidth
+          variant='outlined'
+          onChange={e => setContent(e.target.value)}
+        />
       </div>
       <div>
-        <Button className={classes.button} variant='contained' color='primary'>
+        <Button onClick={handleDeleteDraft} className={classes.button} variant='contained' color='primary'>
           <ClearIcon className={classes.leftIcon} />
           Discard
         </Button>
-        <Button type='submit' className={classes.button} variant='contained' color='secondary'>
+        <Button
+          type='submit'
+          className={classes.button}
+          variant='contained'
+          color='secondary'
+          disabled={!title.trim() || !content.trim() || !image}
+          onClick={handleSubmit}
+        >
           Submit
           <SaveIcon className={classes.rightIcon} />
         </Button>
