3be7dd45f71b3f9f5974d7ce8a9f88d73facda9d
Leila <nb.leila10@gmail.com>
2019-05-16 12:11:38 -0500

    Add playground to check server

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   cors: false,
-  // introspection: true,
-  // playground: true,
+  introspection: true,
+  playground: true,
   context: async ({ req }) => {
     let authToken = null;
     let currentUser = null;
